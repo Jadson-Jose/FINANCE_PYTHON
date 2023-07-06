b01989ac8d472851fd0ae81a721238dbd5fc3a1d
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2023-07-05 22:46:23 -0300

    PSW day 2 part 1

---
+++ extrato/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('novo_valor/', views.novo_valor, name="novo_valor"),
+    path('view_extrato/', views.view_extrato, name="view_extrato"),
+]
---
+++ extrato/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from perfil.models import Categoria, Conta
+from django.http import HttpResponse
+from .models import Valores
+from django.contrib import messages
+from django.contrib.messages import constants
+from datetime import datetime
+
+# Create your views here.
+
+
+def novo_valor(request):
+    if request.method == 'GET':
+        contas = Conta.objects.all()
+        categorias = Categoria.objects.all()
+        return render(request, 'nono_valor.html', {'contas': contas, 'categorias': categorias})
+    elif request.method == 'POST':
+        valor = request.POST.get('valor')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        conta = request.POST.get('conta')
+        tipo = request.POST.get('tipo')
+
+        valores = Valores(
+            valor=valor,
+            categoria_id=categoria,
+            descricao=descricao,
+            data=data,
+            conta_id=conta,
+            tipo=tipo
+        )
+
+        valores.save()
+
+        conta = Conta.objects.get(id=conta)
+        if tipo == 'E':
+            conta.valor += int(valor)
+        else:
+            conta.valor -= int(valor)
+
+        conta.save()
+
+        # TODO Mensagem processada de acordo com o tipo
+
+        messages.add_message(request, constants.SUCCESS,
+                             "Entrada/Saída cadastrada com sucesso.")
+        return redirect('/extrato/novo_valor')
+
+
+def view_extrato(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    conta_get = request.GET.get('conta')
+    categoria_get = request.GET.get('categoria')
+
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+
+    if conta_get:
+        valores = valores.filter(conta__id=conta_get)
+
+    if categoria_get:
+        valores = valores.filter(categoria__id=categoria_get)
+
+    # TODO Botao para zerar os filtros
+    # TODO Filtrar por periodos
+
+    return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
